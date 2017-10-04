fa3d5951146b33cc826bf7454a6567fc792c13b0
Badra <badra95100@gmail.com>
2017-10-04 22:39:02 +0200

    avancement

--- js/functions.js
+++ js/functions.js
@@ -1,70 +1,134 @@
 function RectangleLargerSide( width, height ) {
 	/* Le bord plus longue d'un rectangle */
-	return;
+	if (width>height){
+		return width;
+	}
+	return height;
+	// return Math.max (width, height);
 }
+
 function TriangleLargestSide( a, b, c ) {
 	/* Le bord le plus longue d'un triangle */
-	return;
+	// 	if (a >= b && a >= c){
+	// 		return a;
+	// 	};
+	// 	if (b >= a && b >= c){
+	// 		return b;
+	// 	};
+	// 	else (c >= a && c >= b)
+	// 	return Math.max (a, b, c);
+	// }
+	return Math.max (a, b, c);
 }
+
 function checkIfRectangleIsSquare( width, height ) {
 	/* vérifie si rectangle est carré */
-	return;
+	return width===height;
 }
+
 function checkIfHrefStartsWithHash( string ) {
 	/* vérifie si le lien commence par hash (#) */
-	return;
+	// if (string.charAt(0)==="#"){
+	// 	return true;
+	// }
+	// else {
+	// 	return false;
+	// }
+	if (string.startsWith("#")){
+		return true;
+	}else{
+		return false;
+	}
 }
+
 function checkIfFileIsPNGFile( string ) {
 	/* vérifie si le fichier est un png */
-	return;
+	if (string.endsWith(".png") || string.endsWith(".PNG")){
+		return true;
+	}else{
+		return false;
+	}
 }
 
 function selectElementsStartingWithA(array) {
 	/* choisis tous les élémenent commencant par a ou A */
-	return;
-};
+	var tab = [];
+	for (i=0; i < array.length; i++){
+		var elementarray = array[i];
+		if (elementarray.charAt(0)==="a" || elementarray.charAt(0)==="A"){
+			tab.push(elementarray);
+		}
+	}
+	return tab;
+}
 
 function allElementsExceptFirstThree(array) {
 	/* choisis tous les élémenent sauf les trois premières */
-	return;
-};
+	var tab = [];
+	for (var i=3; i<array.length; i++){
+		tab.push(array[i]);
+	}
+	return tab;
+}
 
 function addElementToBeginning(array, element) {
 	/* ajout un élément au début du tableau array */
-	return;
+	// array.splice(0, 0, "element");
+	array.unshift(element);
+	return array;
 };
 
 function removeElementAtGivenPosition(array, position) {
 	/* supprime l'élément en position donnée */
-	return;
+	array.splice(position, 1);
+	return array;
 };
 
 function removeLastElement(array) {
 	/* supprime le dernier 'élément */
-	return;
+	// array.splice(array.length - 1, 1);
+	array.pop();
+	return array;
 };
-
 
 function makeNegative(number) {
 	/* un nombre négative (ex. 5 -> -5, mais -5 -> -5) */
-	return;
+	if (number > 0){
+		number *= -1;
+	};
+	return number;
 };
-
 
 function sumNumbers(array) {
 	/* calcule la somme de tous les éléments dans le tableau */
-	return;
+	var result = 0;
+	for (var i=0; i<array.length; i++){
+		result += array[i];
+	}
+	return result;
 };
 
 function calculateAverage(array) {
 	/* calcule la moyenne de tous les éléments dans le tableau */
-	return;
+	var result = 0;
+	for (var i=0; i<array.length; i++){
+		result += array[i];
+	}
+	result /= array.length;
+	return result;
 };
-
 
 function longestWord(array) {
 	/* choisis le mot le plus longue dans le tableau */
-	return;
+	var mot = 0;
+	var mots = "";
+	for (var i=0; i<array.length; i++) {
+		if(array[i].length > mot){
+			mot = array[i].length;
+			mots = array[i];
+		};
+	}
+	return mots;
 };
 
 function getAllLetters(array) {
